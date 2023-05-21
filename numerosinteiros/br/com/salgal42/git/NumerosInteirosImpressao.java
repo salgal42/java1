b79c16efa8471a8940c4df3a8ce750773649ebcb
Salatiel Galvão Zambelino <84423199+salgal42@users.noreply.github.com>
2023-05-21 16:02:39 -0300

    Adicionei arquivos via upload

---
+++ numerosinteiros/br/com/salgal42/git/NumerosInteirosImpressao.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package br.com.salgal42.git;
+import java.util.Scanner;
+/**
+ * @author Salatiel Galvão Zambelino
+ * Leitura e impressão de dois números inteiros
+ */
+public class NumerosInteirosImpressao {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n1, n2;
+		
+		n1 = sc.nextInt();
+		
+		n2 = sc.nextInt();
+		
+		System.out.println(n1);
+		
+		System.out.println(n2);
+		
+		sc.close();
+	
+
+	}
+
+}
